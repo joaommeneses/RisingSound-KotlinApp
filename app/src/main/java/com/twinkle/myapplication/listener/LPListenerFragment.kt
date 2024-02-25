88b5316c0a33fdba191779f891562d4afbfd8f2b
João Santos <joaomeneses1804@gmail.com>
2024-02-25 12:22:10 +0000

    feat: landing page - video with slide

--- app/src/main/java/com/twinkle/myapplication/listener/LPListenerFragment.kt
+++ app/src/main/java/com/twinkle/myapplication/listener/LPListenerFragment.kt
@@ -5,24 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.twinkle.myapplication.R
+import com.twinkle.myapplication.adapters.VideosPagerAdapter
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LPListenerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LPListenerFragment : Fragment() {
+    private lateinit var viewPager: ViewPager2
+    private val videosList = listOf(R.raw.example, R.raw.example, R.raw.example) // same video for now
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_land_page_listener, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewPager = view.findViewById(R.id.videosViewPager)
+        viewPager.adapter = VideosPagerAdapter(this)
+        viewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
+    }
 }
+
